c949195a998be540c2c7295b085289dac3391939
Tony Creavin <36261250+TonyCreavin@users.noreply.github.com>
2022-11-07 13:00:34 +0100

    Add files via upload
    added user and movie

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-const port = process.env.APP_PORT ?? 5000;
+app.use(express.json());
+
+const port = process.env.APP_PORT ?? 5050;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send('Welcome to my favourite movie list');
 };
 
-app.get("/", welcome);
+app.get('/', welcome);
 
-const movieHandlers = require("./movieHandlers");
-const userHandler = require("./userHandler");
+const movieHandlers = require('./movieHandlers');
+const userHandler = require('./userHandler');
 
-app.get("/api/movies-json", movieHandlers.getMoviesFromJson);
-app.get("/api/movies-json/:id", movieHandlers.getMovieFromJsonById);
+app.post('/api/movies', movieHandlers.postMovies);
+app.get('/api/movies-json', movieHandlers.getMoviesFromJson);
+app.get('/api/movies-json/:id', movieHandlers.getMovieFromJsonById);
 
-app.get("/api/movies", movieHandlers.getMoviesFromDB);
-app.get("/api/movies/:id", movieHandlers.getMovieFromDbById);
+app.get('/api/movies', movieHandlers.getMoviesFromDB);
+app.get('/api/movies/:id', movieHandlers.getMovieFromDbById);
 
-app.get("/api/users", userHandler.getUsers);
-app.get("/api/users/:id", userHandler.getUserById);
+app.post('/api/users', userHandler.postUsers);
+app.get('/api/users', userHandler.getUsers);
+app.get('/api/users/:id', userHandler.getUserById);
 
 app.listen(port, (err) => {
   if (err) {
-    console.error("Something bad happened");
+    console.error('Something bad happened');
   } else {
     console.log(`Server is listening on ${port}`);
   }
--- userHandler.js
+++ userHandler.js
@@ -1,15 +1,32 @@
-const database = require("./database");
+const database = require('./database');
+
+const postUsers = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      'INSERT INTO users (firstname, lastname, email, city, language) VALUES ( ?, ?, ?, ?, ?)',
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.status(201).json(result.insertId);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Cannot load contact');
+    });
+};
 
 const getUsers = (req, res) => {
   database
-    .query("select * from users")
+    .query('select * from users')
     .then((result) => {
       console.log(result[0]);
       res.json(result[0]);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send("Error retrieving users from database");
+      res.status(500).send('Error retrieving users from database');
     });
 };
 
@@ -17,22 +34,23 @@ const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-    .query("select * from users where id = ?", [id])
+    .query('select * from users where id = ?', [id])
     .then((result) => {
       console.log(result);
       if (result[0] != null) {
         res.json(result[0]);
       } else {
-        res.status(404).send("Not Found");
+        res.status(404).send('Not Found');
       }
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send("Error retrieving data from database");
+      res.status(500).send('Error retrieving data from database');
     });
 };
 
 module.exports = {
   getUsers,
   getUserById,
+  postUsers,
 };
